e937c5f9b5ffb34245c7f442021b9f2a90f297c8
William Beckett <william.beckett@sunwater.com.au>
2017-10-25 14:15:14 +1000

    testing scipt changes

--- init/01_update.sh
+++ init/01_update.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bash
+echo "updating packages"
 apk update
 apk upgrade
-cd /mp4automator
-git pull
+echo "grabbing latest mp4automator"
+[[ ! -d /mp4automator/.git ]] && (git clone git://github.com/mdhiggins/sickbeard_mp4_automator.git /mp4automator && chown -R abc:abc /mp4automator)
+echo "setting up mp4Automator"
+[[ ! -f /mp4automator/autoProcess.ini ]] && (cp /mp4automator/autoProcess.ini.sample /mp4automator/autoProcess.ini)
+
+echo "updating permissions"
 chmod 775 -R /mp4automator
+
+echo "installing pip packages"
 pip install --upgrade PIP
 pip install --upgrade requests
 pip install --upgrade requests[security]
